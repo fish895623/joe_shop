ea2327ee5da57ae1293a95371f9f1901da685a8f
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-21 17:28:45 +0900

    First Commit

---
+++ src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.joe.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
