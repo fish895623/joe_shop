3dd64029235aa755939369404312c4233fbaaa59
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-24 15:44:12 +0900

    chore(testing): Container testing

--- src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
+++ src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
@@ -1,11 +1,24 @@
 package com.bit.joe.shoppingmall;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BitApplicationTests {
+    @Container
+    public static MySQLContainer<?> mySQLContainer =
+            new MySQLContainer<>("mysql:5.7")
+                    .withExposedPorts(3306)
+                    .withDatabaseName("shoppingmall")
+                    .withUsername("bit")
+                    .withPassword("1234");
 
-    // TODO: setup container testing environment
-    // @Test
-    // void contextLoads() {}
+    @Test
+    void contextLoads() {
+        assert mySQLContainer.isRunning();
+    }
 }
