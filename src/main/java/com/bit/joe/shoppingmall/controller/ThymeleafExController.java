5b4a83b01871ab836cd7b722f1137156426fd501
dangew <dangew99@gmail.com>
2025-02-24 16:48:05 +0900

    execute ./gradlew spotlessApply

--- src/main/java/com/bit/joe/shoppingmall/controller/ThymeleafExController.java
+++ src/main/java/com/bit/joe/shoppingmall/controller/ThymeleafExController.java
@@ -1,42 +1,42 @@
 package com.bit.joe.shoppingmall.controller;
 
-import com.bit.joe.shoppingmall.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.bit.joe.shoppingmall.entity.User;
 
 @Controller
 @RequestMapping("thymeleaf")
 public class ThymeleafExController {
 
-  @GetMapping("/ex1")
-  public String thymeleaf(Model model) {
-    model.addAttribute("pageTitle", "Using th:text example");
-    model.addAttribute("data", "Hello, Thymeleaf!");
+    @GetMapping("/ex1")
+    public String thymeleaf(Model model) {
+        model.addAttribute("pageTitle", "Using th:text example");
+        model.addAttribute("data", "Hello, Thymeleaf!");
 
-    return "thymeleaf/thymeleaf";
-  }
-
-  @GetMapping("/ex2")
-  public String thymeleaf2(Model model) {
-    List<User> users = new ArrayList<>();
-
-    for (int i = 0; i < 10; i++) {
-      User user = new User();
-      user.setName("sameple user" + i);
-      user.setEmail(i + "@" + i);
-
-      users.add(user);
+        return "thymeleaf/thymeleaf";
     }
 
-    model.addAttribute("pageTitle", "Using th:each example");
-    model.addAttribute("users", users);
+    @GetMapping("/ex2")
+    public String thymeleaf2(Model model) {
+        List<User> users = new ArrayList<>();
 
-    return "thymeleaf/thymeleaf2";
-  }
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setName("sameple user" + i);
+            user.setEmail(i + "@" + i);
 
+            users.add(user);
+        }
+
+        model.addAttribute("pageTitle", "Using th:each example");
+        model.addAttribute("users", users);
+
+        return "thymeleaf/thymeleaf2";
+    }
 }
