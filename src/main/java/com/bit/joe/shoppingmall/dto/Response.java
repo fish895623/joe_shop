f60996082b055f3563b6ec1d008adc8342dc558a
JUNGHYUN MIN <69973910+KatherineJH@users.noreply.github.com>
2025-02-24 20:12:26 +0900

    add packages (#8)
    * add packages
    
    * chore(convention): spotless

---
+++ src/main/java/com/bit/joe/shoppingmall/dto/Response.java
@@ -0,0 +1,37 @@
+package com.bit.joe.shoppingmall.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+
+    // "응답"시의 DTO
+    private int status;
+    private String message;
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private String role;
+
+    private CategoryDto category;
+    private List<CategoryDto> categoryList;
+
+    private ProductDto product;
+    private List<ProductDto> productList;
+
+    //    private UserDto user;
+    //    private List<UserDto> userList;
+
+    //    private OrderItemDto orderItem;
+    //    private List<OrderItemDto> orderItemList;
+
+    //    private OrderDto order;
+    //    private List<OrderDto> orderList;
+
+}
