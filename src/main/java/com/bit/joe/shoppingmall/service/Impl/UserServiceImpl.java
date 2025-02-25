ae6f9329236864c3202de7fa3f10b59473b16f3a
dangew <dangew99@gmail.com>
2025-02-25 12:20:20 +0900

    execute ./gradlew spotlessApply

--- src/main/java/com/bit/joe/shoppingmall/service/Impl/UserServiceImpl.java
+++ src/main/java/com/bit/joe/shoppingmall/service/Impl/UserServiceImpl.java
@@ -1,19 +1,19 @@
 package com.bit.joe.shoppingmall.service.Impl;
 
-import com.bit.joe.shoppingmall.entity.User;
-import com.bit.joe.shoppingmall.exception.NotFoundException;
-import com.bit.joe.shoppingmall.mapper.UserMapper;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.bit.joe.shoppingmall.dto.Response;
 import com.bit.joe.shoppingmall.dto.UserDto;
+import com.bit.joe.shoppingmall.entity.User;
+import com.bit.joe.shoppingmall.exception.NotFoundException;
+import com.bit.joe.shoppingmall.mapper.UserMapper;
 import com.bit.joe.shoppingmall.repository.UserRepository;
 import com.bit.joe.shoppingmall.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Service
 @Slf4j
@@ -36,7 +36,9 @@ public class UserServiceImpl implements UserService {
     @Override
     public Response updateUser(Long userId, UserDto userRequest) {
 
-        userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User data not found"));
+        userRepository
+                .findById(userId)
+                .orElseThrow(() -> new NotFoundException("User data not found"));
         // Check if user exists
         User user = UserMapper.toEntity(userRequest);
         // Convert UserDto to User
@@ -51,7 +53,9 @@ public class UserServiceImpl implements UserService {
     @Override
     public Response deleteUser(Long userId) {
 
-        userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User data not fount"));
+        userRepository
+                .findById(userId)
+                .orElseThrow(() -> new NotFoundException("User data not fount"));
         // Check if user exists
         userRepository.deleteById(userId);
         // Delete user
@@ -62,22 +66,36 @@ public class UserServiceImpl implements UserService {
     @Override
     public Response getUserById(Long userId) {
 
-        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User data not found"));
+        User user =
+                userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new NotFoundException("User data not found"));
         // Check if user exists and get user
         UserDto userDto = UserMapper.toDto(user);
         // Convert User to UserDto
-        return Response.builder().status(200).message("User data retrieved successfully").user(userDto).build();
+        return Response.builder()
+                .status(200)
+                .message("User data retrieved successfully")
+                .user(userDto)
+                .build();
         // return success response
     }
 
     @Override
     public Response getUserByEmail(String email) {
 
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("User data not found"));
+        User user =
+                userRepository
+                        .findByEmail(email)
+                        .orElseThrow(() -> new NotFoundException("User data not found"));
         // Check if user exists and get user
         UserDto userDto = UserMapper.toDto(user);
         // Convert User to UserDto
-        return Response.builder().status(200).message("User data retrieved successfully").user(userDto).build();
+        return Response.builder()
+                .status(200)
+                .message("User data retrieved successfully")
+                .user(userDto)
+                .build();
         // return success response
     }
 
@@ -86,7 +104,11 @@ public class UserServiceImpl implements UserService {
 
         List<UserDto> userList = userRepository.findAll().stream().map(UserMapper::toDto).toList();
         // Get all users and convert them to UserDto
-        return Response.builder().status(200).message("All users retrieved successfully").userList(userList).build();
+        return Response.builder()
+                .status(200)
+                .message("All users retrieved successfully")
+                .userList(userList)
+                .build();
         // reuturn success response
     }
 
