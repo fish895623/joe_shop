3f83f98e0e5dadb24f48bdf026a3c2ced7e88d26
dangew <dangew99@gmail.com>
2025-02-25 10:25:46 +0900

    Setup Package of control, service for User

---
+++ src/main/java/com/bit/joe/shoppingmall/service/UserService.java
@@ -0,0 +1,23 @@
+package com.bit.joe.shoppingmall.service;
+
+import com.bit.joe.shoppingmall.dto.Response;
+import com.bit.joe.shoppingmall.dto.UserDto;
+
+public interface UserService {
+  Response createUser(UserDto userRequest);
+
+  Response updateUser(Long userId, UserDto userRequest);
+
+  Response getAllUsers();
+
+  Response getUserById(Long userId);
+
+  Response getUserByEmail(String email);
+
+  Response deleteUser(Long userId);
+
+  Response login(String email, String password);
+
+  Response logout(String email);
+
+}
