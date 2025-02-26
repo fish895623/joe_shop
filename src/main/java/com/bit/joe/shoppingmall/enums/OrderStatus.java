7089e6f942db80a7ad9ae685d790bf1966aa0d1a
dangew <dangew99@gmail.com>
2025-02-26 17:19:21 +0900

    WIP

--- src/main/java/com/bit/joe/shoppingmall/enums/OrderStatus.java
+++ src/main/java/com/bit/joe/shoppingmall/enums/OrderStatus.java
@@ -1,5 +1,12 @@
 package com.bit.joe.shoppingmall.enums;
 
 public enum OrderStatus {
-    ORDER,    CONFIRM,    PREPARE,    DELIVER,    DELIVERED,    RETURN_REQUESTED,    RETURN_IN_PROGRESS,    RETURNED
+    ORDER,
+    CONFIRM,
+    PREPARE,
+    DELIVER,
+    DELIVERED,
+    RETURN_REQUESTED,
+    RETURN_IN_PROGRESS,
+    RETURNED,
 }
