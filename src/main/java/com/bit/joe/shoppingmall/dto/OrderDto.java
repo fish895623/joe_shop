1c82576cb88467b6c67e4ea158e943b592819769
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-26 11:47:35 +0900

    feat: Add new DTO classes and mappers

---
+++ src/main/java/com/bit/joe/shoppingmall/dto/OrderDto.java
@@ -0,0 +1,29 @@
+package com.bit.joe.shoppingmall.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.bit.joe.shoppingmall.entity.OrderItem;
+import com.bit.joe.shoppingmall.entity.User;
+import com.bit.joe.shoppingmall.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+    private Long id;
+    private User user;
+    private String orderDate;
+    private OrderStatus status;
+    private LocalDateTime createdAt;
+    private List<OrderItem> orderItems; // 수정: orderLists -> orderItems
+}
---
+++ src/main/java/com/bit/joe/shoppingmall/mapper/OrderItemMapper.java
@@ -0,0 +1,7 @@
+package com.bit.joe.shoppingmall.mapper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderItemMapper {
+}
