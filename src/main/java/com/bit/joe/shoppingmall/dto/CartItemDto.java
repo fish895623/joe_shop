b084db53fd0610515cd432370b39a4ddbc295b11
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-26 16:22:24 +0900

    feat: add dto for catitem

---
+++ src/main/java/com/bit/joe/shoppingmall/dto/CartItemDto.java
@@ -0,0 +1,21 @@
+package com.bit.joe.shoppingmall.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDto {
+    private Long id;
+    private CartDto cart;
+    private ProductDto product;
+}
