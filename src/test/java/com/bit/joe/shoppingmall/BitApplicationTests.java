c527894ccf3312cdea422c925f538f982528b7f2
bsh <dan990429@gmail.com>
2025-02-28 01:10:47 +0900

    B

--- src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
+++ src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
@@ -2,15 +2,17 @@ package com.bit.joe.shoppingmall;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@PropertySource("classpath:application.test.properties")
 class DemoApplicationTests {
-    private static final String USERNAME = "username";
-    private static final String PASSWORD = "password";
-    private static final String DATABASE_NAME = "testdb";
+    private static final String USERNAME = "bit";
+    private static final String PASSWORD = "1234";
+    private static final String DATABASE_NAME = "shoppingmall";
 
     static MySQLContainer<?> mySQLContainer =
             new MySQLContainer<>("mysql:8.0")
