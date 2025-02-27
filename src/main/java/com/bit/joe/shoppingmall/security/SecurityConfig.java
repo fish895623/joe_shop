a3af57ffd89ee71210a40cf3990f2c0eca4624d3
dangew <dangew99@gmail.com>
2025-02-27 15:30:39 +0900

    refactor: fix warning about authentication

--- src/main/java/com/bit/joe/shoppingmall/security/SecurityConfig.java
+++ src/main/java/com/bit/joe/shoppingmall/security/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.bit.joe.shoppingmall.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,21 +16,20 @@ import com.bit.joe.shoppingmall.enums.UserRole;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomAuthenticationProvider customAuthenticationProvider;
-
-    public SecurityConfig(CustomAuthenticationProvider customAuthenticationProvider) {
-        this.customAuthenticationProvider = customAuthenticationProvider;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        request ->
-                                request.requestMatchers("/user/get-all")
-                                        .hasAuthority(UserRole.ADMIN.name())
-                                        .anyRequest()
-                                        .permitAll())
+                        request -> request
+                            // add more requestMatchers here to restrict access to certain endpoints
+                            .requestMatchers("/user/get-all")
+                            .hasAuthority(UserRole.ADMIN.name())
+                            .requestMatchers("/category/create")
+                            .hasAuthority(UserRole.ADMIN.name())
+                            .requestMatchers("/product/create")
+                            .hasAuthority(UserRole.ADMIN.name())
+                            .anyRequest()
+                            .permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
@@ -41,15 +38,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder =
-                http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider);
-        return authenticationManagerBuilder.build();
-    }
-
-    @Bean
-    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
+
 }
