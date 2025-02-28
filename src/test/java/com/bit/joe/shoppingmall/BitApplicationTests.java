72f03f353a3751a59bc7164210af501b51458112
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-02-28 11:53:26 +0900

    feat: Add dynamic properties configuration for MySQLContainer

--- src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
+++ src/test/java/com/bit/joe/shoppingmall/BitApplicationTests.java
@@ -3,6 +3,8 @@ package com.bit.joe.shoppingmall;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -13,6 +15,14 @@ class DemoApplicationTests {
     private static final String USERNAME = "bit";
     private static final String PASSWORD = "1234";
     private static final String DATABASE_NAME = "shoppingmall";
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", mySQLContainer::getDriverClassName);
+    }
 
     static MySQLContainer<?> mySQLContainer =
             new MySQLContainer<>("mysql:lts")
