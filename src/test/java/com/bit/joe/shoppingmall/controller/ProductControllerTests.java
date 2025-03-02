7c455fd0ead372eaa73792ad23f615c563f20825
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-03-02 17:47:30 +0900

    style: improve code formatting and imports

--- src/test/java/com/bit/joe/shoppingmall/controller/ProductControllerTests.java
+++ src/test/java/com/bit/joe/shoppingmall/controller/ProductControllerTests.java
@@ -1,20 +1,50 @@
 package com.bit.joe.shoppingmall.controller;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import java.util.Base64;
+
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.testcontainers.containers.MySQLContainer;
 
 import com.bit.joe.shoppingmall.config.MySQLContainerConfig;
+import com.bit.joe.shoppingmall.dto.UserDto;
+import com.bit.joe.shoppingmall.enums.UserGender;
+import com.bit.joe.shoppingmall.enums.UserRole;
+import com.bit.joe.shoppingmall.service.ProductService;
+
+import jakarta.servlet.http.HttpSession;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductControllerTests {
     static final MySQLContainer<?> mysql = MySQLContainerConfig.getInstance();
+    UserDto userDto =
+            UserDto.builder()
+                    .name("admin")
+                    .password("admin")
+                    .email("admin@example.com")
+                    .role(UserRole.ADMIN)
+                    .gender(UserGender.MALE)
+                    .birth("2021-01-01")
+                    .build();
+    String basicAuthHeader =
+            "Basic "
+                    + Base64.getEncoder()
+                            .encodeToString(
+                                    (userDto.getEmail() + userDto.getPassword()).getBytes());
+    MockHttpSession mockHttpSession = new MockHttpSession();
+    private MockMvc mockMvc;
+    @Mock private ProductService productService;
+    @Mock private HttpSession session;
+    @InjectMocks private ProductController productController;
 
-    @Test
-    @Order(1)
-    void test1() {
-        System.out.println("test1");
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
     }
 }
