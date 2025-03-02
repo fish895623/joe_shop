3bc35835ae0be46e681b740a0da01263d6d6f855
dangew <dangew99@gmail.com>
2025-03-02 19:00:50 +0900

    chore: setting for [order, orderItem api]

--- src/main/java/com/bit/joe/shoppingmall/dto/OrderDto.java
+++ src/main/java/com/bit/joe/shoppingmall/dto/OrderDto.java
@@ -3,17 +3,19 @@ package com.bit.joe.shoppingmall.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.bit.joe.shoppingmall.entity.CartItem;
+import com.bit.joe.shoppingmall.entity.OrderItem;
 import com.bit.joe.shoppingmall.entity.User;
 import com.bit.joe.shoppingmall.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
@@ -21,8 +23,7 @@ import lombok.NoArgsConstructor;
 public class OrderDto {
     private Long id;
     private User user;
-    private String orderDate;
+    private LocalDateTime orderDate;
     private OrderStatus status;
-    private LocalDateTime createdAt;
-    private List<CartItem> orderItems; // 수정: orderLists -> orderItems
+    private List<OrderItem> orderItems; // 수정: orderLists -> orderItems
 }
--- src/main/java/com/bit/joe/shoppingmall/entity/OrderItem.java
+++ src/main/java/com/bit/joe/shoppingmall/entity/OrderItem.java
@@ -3,9 +3,17 @@ package com.bit.joe.shoppingmall.entity;
 import java.math.BigDecimal;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "order_items")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItem {
 
     @Id
