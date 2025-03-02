a38608a3cab2d2fa205ccab4f0267c5752cf5d02
dangew <dangew99@gmail.com>
2025-03-02 20:39:19 +0900

    feat: /api/order/create

--- src/main/java/com/bit/joe/shoppingmall/controller/OrderController.java
+++ src/main/java/com/bit/joe/shoppingmall/controller/OrderController.java
@@ -1,8 +1,13 @@
 package com.bit.joe.shoppingmall.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bit.joe.shoppingmall.dto.request.OrderRequest;
+import com.bit.joe.shoppingmall.dto.response.Response;
 import com.bit.joe.shoppingmall.service.Impl.OrderService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,4 +20,9 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderController {
 
     private final OrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Response> createOrder(@RequestBody OrderRequest orderRequest) {
+        return ResponseEntity.ok(orderService.createOrder(orderRequest));
+    }
 }
