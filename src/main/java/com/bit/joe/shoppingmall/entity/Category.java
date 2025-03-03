fefa1004a07a70a084b023d8bdf4f78e7c2e2af6
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-03-04 00:08:05 +0900

    feat: Add Lombok annotations to Category entity and refactor CategoryServiceTests

--- src/main/java/com/bit/joe/shoppingmall/entity/Category.java
+++ src/main/java/com/bit/joe/shoppingmall/entity/Category.java
@@ -3,11 +3,17 @@ package com.bit.joe.shoppingmall.entity;
 import java.util.List;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "categories")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
