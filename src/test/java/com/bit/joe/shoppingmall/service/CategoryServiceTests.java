56ddc18d9a2cb03a85161f020e64b6787e2b27c2
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-03-04 12:01:45 +0900

    test: CategoryService TestCase (#59)
    * feat: Add Lombok annotations to Category entity and refactor CategoryServiceTests
    
    * refactor: Simplify test configuration and adjust coverage minimum in build.gradle
    
    * test: Refactor CategoryControllerTests to use Spring Boot Test and Testcontainers
    
    * test: Enhance CategoryControllerTests with additional category retrieval scenarios
    
    * test: Update CategoryControllerTests to set category ID before saving

--- src/test/java/com/bit/joe/shoppingmall/service/CategoryServiceTests.java
+++ src/test/java/com/bit/joe/shoppingmall/service/CategoryServiceTests.java
@@ -1,66 +1,221 @@
 package com.bit.joe.shoppingmall.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import com.bit.joe.shoppingmall.dto.CategoryDto;
 import com.bit.joe.shoppingmall.dto.response.Response;
+import com.bit.joe.shoppingmall.entity.Category;
 import com.bit.joe.shoppingmall.repository.CategoryRepository;
-import com.bit.joe.shoppingmall.repository.ProductRepository;
 import com.bit.joe.shoppingmall.service.Impl.CategoryServiceImpl;
 
+import jakarta.transaction.Transactional;
+
+@ExtendWith({SpringExtension.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Testcontainers(parallel = true)
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Testcontainers
 class CategoryServiceTests {
 
-    @Mock private ProductRepository productRepository;
-    @Mock private CategoryRepository categoryRepository;
-    @InjectMocks private CategoryServiceImpl categoryService;
+    @Container public static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:lts");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.MySQLDialect");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+        registry.add("spring.jpa.show-sql", () -> "true");
+    }
+
+    @Autowired private CategoryServiceImpl categoryService;
+
+    @Autowired private CategoryRepository categoryRepository;
 
     @BeforeEach
-    void setUp() {
-        categoryRepository = Mockito.mock(CategoryRepository.class);
-        productRepository = Mockito.mock(ProductRepository.class);
-        MockitoAnnotations.openMocks(this);
+    public void setUp() {
+        categoryRepository.deleteAll();
     }
 
     @Test
-    @Order(1)
-    void testCreateCategory() {
+    @Transactional
+    public void createCategory() {
         CategoryDto categoryDto = new CategoryDto();
-        categoryDto.setCategoryName("test");
+        categoryDto.setCategoryName("Electronics");
+
         Response response = categoryService.createCategory(categoryDto);
 
-        Assertions.assertEquals(200, response.getStatus());
+        assertEquals(200, response.getStatus());
+        assertEquals("Category created successfully", response.getMessage());
 
-        Assertions.assertEquals("test", categoryRepository.findById(1L).get().getCategoryName());
+        var category = categoryRepository.findAll();
+        System.out.println(category);
     }
 
     @Test
-    @Order(2)
-    void testUpdateCategory() {
+    @Transactional
+    public void updateCategory() {
+        // Given
+        Category category = new Category();
+        category.setCategoryName("Electronics");
+        categoryRepository.save(category);
+
+        CategoryDto updateCategoryDto = new CategoryDto();
+        updateCategoryDto.setCategoryName("Home Appliances");
+
+        // When
+        Response response = categoryService.updateCategory(category.getId(), updateCategoryDto);
+
+        // Then
+        assertEquals(200, response.getStatus());
+
+        Optional<Category> updatedCategory = categoryRepository.findById(category.getId());
+        assertTrue(updatedCategory.isPresent());
+        assertEquals("Home Appliances", updatedCategory.get().getCategoryName());
+    }
+
+    @Test
+    @Transactional
+    public void getAllCategory() {
+        // Given
+        Category category1 = new Category();
+        category1.setCategoryName("Electronics");
+        categoryRepository.save(category1);
+
+        Category category2 = new Category();
+        category2.setCategoryName("Home Appliances");
+        categoryRepository.save(category2);
+
+        // When
+        Response response = categoryService.getAllCategories();
+
+        // Then
+        assertEquals(200, response.getStatus());
+        assertEquals(2, response.getCategoryList().size());
+    }
+
+    @Test
+    @Transactional
+    public void getCategoryById() {
+        // Given
+        Category category = new Category();
+        category.setCategoryName("Electronics");
+        categoryRepository.save(category);
+
+        // When
+        Response response = categoryService.getCategoryById(category.getId());
+
+        // Then
+        assertEquals(200, response.getStatus());
+        assertEquals("Electronics", response.getCategory().getCategoryName());
+    }
+
+    @Test
+    @Transactional
+    public void deleteCategory() {
+        // Given
+        Category category = new Category();
+        category.setCategoryName("Electronics");
+        categoryRepository.save(category);
+
+        // When
+        Response response = categoryService.deleteCategory(category.getId());
+
+        // Then
+        assertEquals(200, response.getStatus());
+        assertEquals("Category was deleted successfully", response.getMessage());
+    }
+
+    @Test
+    @Transactional
+    public void failedFindById() {
+        // Given
+        Category category = new Category();
+        category.setCategoryName("Electronics");
+        categoryRepository.save(category);
+
+        // When Response status is 400
+        Response response = categoryService.getCategoryById(100L);
+
+        // Then
+        assertTrue(response.getStatus() >= 400 || response.getStatus() < 500);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistCategory() {
+        // Given
+        Category category = new Category();
+        category.setCategoryName("Electronics");
+        categoryRepository.save(category);
+
+        CategoryDto updateCategoryDto = new CategoryDto();
+        updateCategoryDto.setCategoryName("Home Appliances");
+
+        // When Response status is 400
+        Response response = categoryService.updateCategory(100L, updateCategoryDto);
+
+        // Then
+        assertTrue(response.getStatus() >= 400 || response.getStatus() < 500);
+    }
+
+    @Test
+    @Transactional
+    public void deleteNonExistCategory() {
+        // Given
+        Category category = new Category();
+        category.setCategoryName("Electronics");
+        categoryRepository.save(category);
+
+        // When Response status is 400
+        Response response = categoryService.deleteCategory(100L);
+
+        // Then
+        assertTrue(response.getStatus() >= 400 || response.getStatus() < 500);
+    }
+
+    @Test
+    @Transactional
+    public void createCategoryWithNullNameAndDuplicate() {
+        // Given
         CategoryDto categoryDto = new CategoryDto();
-        categoryDto.setCategoryName("test2");
-        Response response = categoryService.updateCategory(1L, categoryDto);
+        categoryDto.setCategoryName(null);
 
-        Assertions.assertEquals(200, response.getStatus());
-    }
+        // When Response status is 400
+        Response response = categoryService.createCategory(categoryDto);
 
-    @Test
-    @Order(3)
-    void testUpdateNotExistCategory() {
-        CategoryDto categoryDto = new CategoryDto();
-        categoryDto.setCategoryName("test");
-        categoryService.updateCategory(2L, categoryDto);
-    }
+        // Then
+        assertTrue(response.getStatus() >= 400 || response.getStatus() < 500);
 
-    @Test
-    @Order(4)
-    void testGetAllCategories() {
-        categoryService.getAllCategories();
+        // Given
+        CategoryDto categoryDto2 = new CategoryDto();
+        categoryDto2.setCategoryName("Electronics");
+
+        // When
+        Response response2 = categoryService.createCategory(categoryDto2);
+
+        // Then
+        assertEquals(200, response2.getStatus());
+
+        // When Response status is 400
+        Response response3 = categoryService.createCategory(categoryDto2);
+
+        // Then
+        assertTrue(response3.getStatus() >= 400 || response3.getStatus() < 500);
     }
 }
