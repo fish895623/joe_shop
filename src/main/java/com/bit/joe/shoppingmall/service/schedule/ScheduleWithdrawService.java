2eb6b7905b0c3b1e4d2b14589753262fd90a327f
dangew <dangew99@gmail.com>
2025-03-04 15:27:51 +0900

    follow reivew

--- src/main/java/com/bit/joe/shoppingmall/service/schedule/ScheduleWithdrawService.java
+++ src/main/java/com/bit/joe/shoppingmall/service/schedule/ScheduleWithdrawService.java
@@ -1,11 +1,8 @@
 package com.bit.joe.shoppingmall.service.schedule;
 
-import java.util.List;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.bit.joe.shoppingmall.entity.User;
 import com.bit.joe.shoppingmall.repository.UserRepository;
 
 @Service
@@ -14,19 +11,7 @@ public class ScheduleWithdrawService {
     UserRepository userRepository;
 
     @Scheduled(cron = "0 0 0 1 * *") // every month's 1st day at midnight
-    public void withdraw() {
-        System.out.println("Withdraw developping ...");
-
-        List<User> users = userRepository.findByActive(false);
-        // get all users whoes active is false
-
-        // if there is no user whoes active is false, return
-        if (users.isEmpty()) {
-            return;
-        }
-
-        // we do not need to delete all users whoes active is false
-        // userRepository.deleteAll(users);
-        // delete all users whoes active is false
+    public void withdraw() throws Exception {
+        throw new Exception("Withdraw developping ...");
     }
 }
