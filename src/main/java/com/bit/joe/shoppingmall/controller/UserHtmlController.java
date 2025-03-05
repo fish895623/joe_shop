41340f729677c7481a24a163ff9a40b13b93c48f
이태희 <taeheecafe@naver.com>
2025-03-05 20:48:10 +0900

    userHtml 수정 (#87)
    * userHtml 수정
    usrHtmlController 수정
    
    * updateProfile html 삭제
    
    * index html 삭제
    
    * index html 복구
    
    * UserHtmlController 수정
    
    * UserHtmlController 수정

---
+++ src/main/java/com/bit/joe/shoppingmall/controller/UserHtmlController.java
@@ -0,0 +1,30 @@
+package com.bit.joe.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserHtmlController {
+
+    @GetMapping("/main")
+    public String Main() {
+        return "thymeleaf/user/mainPage";
+    }
+
+    @GetMapping("/login")
+    public String Login() {
+        return "thymeleaf/user/login";
+    }
+
+    @GetMapping("/signUp")
+    public String SignUp() {
+        return "thymeleaf/user/signUp";
+    }
+
+    @GetMapping("/myPage")
+    public String MyPage() {
+        return "thymeleaf/user/myPage";
+    }
+}
