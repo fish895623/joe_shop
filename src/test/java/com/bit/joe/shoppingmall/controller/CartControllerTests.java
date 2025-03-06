949a24846b091a4c1f71d233eae1fc161a04b7b1
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-03-06 17:11:42 +0900

    Test Cart Controller Test Case (#99)
    * Add CartItemControllerTests and update Product entity and mapper
    
    * Add CartControllerTests and refactor CartItemControllerTests for improved structure
    
    * Refactor Category and Product controller tests to use AutoConfigureMockMvc and random test method order
    
    * Refactor CartControllerTests for improved structure and cleanup after tests
    
    * Refactor CartControllerTests to improve structure and add userCreateCart test
    
    * Apply spotlessApply

---
+++ src/test/java/com/bit/joe/shoppingmall/controller/CartControllerTests.java
@@ -0,0 +1,137 @@
+package com.bit.joe.shoppingmall.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Base64;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.bit.joe.shoppingmall.entity.Category;
+import com.bit.joe.shoppingmall.entity.Product;
+import com.bit.joe.shoppingmall.entity.User;
+import com.bit.joe.shoppingmall.enums.UserGender;
+import com.bit.joe.shoppingmall.enums.UserRole;
+import com.bit.joe.shoppingmall.mapper.CategoryMapper;
+import com.bit.joe.shoppingmall.mapper.ProductMapper;
+import com.bit.joe.shoppingmall.mapper.UserMapper;
+import com.bit.joe.shoppingmall.repository.*;
+import com.bit.joe.shoppingmall.service.Impl.CartService;
+import com.bit.joe.shoppingmall.service.Impl.CategoryServiceImpl;
+import com.bit.joe.shoppingmall.service.Impl.ProductServiceImpl;
+import com.bit.joe.shoppingmall.service.UserService;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
+@AutoConfigureMockMvc
+@Transactional
+class CartControllerTests {
+    User adminEntity =
+            User.builder()
+                    .name("admin")
+                    .gender(UserGender.MALE)
+                    .role(UserRole.ADMIN)
+                    .birth("2021-01-01")
+                    .email("admin@example.com")
+                    .password("admin")
+                    .build();
+    User userEntity =
+            User.builder()
+                    .name("user")
+                    .password("user")
+                    .email("user@example.com")
+                    .role(UserRole.USER)
+                    .gender(UserGender.MALE)
+                    .birth("2021-01-01")
+                    .build();
+    String adminBasicAuth =
+            "Basic "
+                    + Base64.getEncoder()
+                            .encodeToString(
+                                    (adminEntity.getEmail() + ":" + adminEntity.getPassword())
+                                            .getBytes());
+    String userBasicAuth =
+            "Basic "
+                    + Base64.getEncoder()
+                            .encodeToString(
+                                    (userEntity.getEmail() + ":" + userEntity.getPassword())
+                                            .getBytes());
+    MockHttpSession mockHttpSession = new MockHttpSession();
+    @Autowired private MockMvc mockMvc;
+    @Autowired private UserRepository userRepository;
+    @Autowired private CategoryRepository categoryRepository;
+    @Autowired private CategoryServiceImpl categoryService;
+    @Autowired private ProductRepository productRepository;
+    @Autowired private ProductServiceImpl productService;
+    @Autowired private CartController cartController;
+    @Autowired private CartService cartService;
+    @Autowired private CartRepository cartRepository;
+    @Autowired private UserService userService;
+    @Autowired private CartItemRepository cartItemRepository;
+
+    @PersistenceContext private EntityManager entityManager;
+
+    @BeforeEach
+    public void setUp() {
+        // Create user
+        userService.createUser(UserMapper.toDto(adminEntity));
+        userService.createUser(UserMapper.toDto(userEntity));
+
+        // Prepare data
+        Category category = Category.builder().id(1L).categoryName("Test Category").build();
+        categoryService.createCategory(CategoryMapper.categoryToDto(category));
+
+        Product product =
+                Product.builder()
+                        .id(1L)
+                        .name("Test Product")
+                        .category(category)
+                        .imageURL("image")
+                        .quantity(10)
+                        .price(1000)
+                        .build();
+        var productDto = ProductMapper.toDto(product);
+
+        productService.createProduct(
+                productDto.getCategory().getId(),
+                productDto.getImageUrl(),
+                productDto.getName(),
+                productDto.getQuantity(),
+                productDto.getPrice());
+
+        categoryRepository.flush();
+    }
+
+    @Test
+    void adminCreateCart() throws Exception {
+        // perform login
+        mockMvc.perform(
+                        post("/api/cart/create")
+                                .header("Authorization", adminBasicAuth)
+                                .content("")
+                                .session(mockHttpSession))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void userCreateCart() throws Exception {
+        // perform login
+        mockMvc.perform(
+                        post("/api/cart/create")
+                                .header("Authorization", userBasicAuth)
+                                .content("")
+                                .session(mockHttpSession))
+                .andExpect(status().isOk());
+    }
+}
