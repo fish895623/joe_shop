66a38a533359cc27d27fa35b44564687b4a55ca3
dangew <dangew99@gmail.com>
2025-03-06 11:52:16 +0900

    refactor: modify cart-item's get-all api

--- src/main/java/com/bit/joe/shoppingmall/controller/CartItemController.java
+++ src/main/java/com/bit/joe/shoppingmall/controller/CartItemController.java
@@ -50,23 +50,19 @@ public class CartItemController {
         return ResponseEntity.status(200).body(cartItemService.getCartItem(cartItemRequest));
     }
 
-    // get all cart items
     @GetMapping("/get-all")
-    public ResponseEntity<Response> getAllCartItems() {
-        log.info("get all cart items");
+    public ResponseEntity<Response> getAllCartItems(@RequestParam(required = false) Long userId) {
 
-        return ResponseEntity.status(200).body(cartItemService.getAllCartItems());
-    }
-
-    // get all cart items by user id
-    @GetMapping("/get-all/{userId}")
-    public ResponseEntity<Response> getAllCartItemsByUserId(@PathVariable Long userId) {
-        log.info("get all cart items by user id");
-
-        return ResponseEntity.status(200)
-                .body(
-                        cartItemService.getCartItems(
-                                CartItemRequest.builder().userId(userId).build()));
+        if (userId != null) {
+            log.info("get all cart items by user id");
+            return ResponseEntity.status(200)
+                    .body(
+                            cartItemService.getCartItems(
+                                    CartItemRequest.builder().userId(userId).build()));
+        } else {
+            log.info("get all cart items");
+            return ResponseEntity.status(200).body(cartItemService.getAllCartItems());
+        }
     }
 
     @GetMapping("/clear")
