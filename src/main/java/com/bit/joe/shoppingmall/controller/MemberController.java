cb1c457d49288080b96534a45033a2b22a33952d
Suu-a <sapark1121@gmail.com>
2025-03-07 11:23:44 +0900

    delete unnecessary code

--- src/main/java/com/bit/joe/shoppingmall/controller/MemberController.java
+++ src/main/java/com/bit/joe/shoppingmall/controller/MemberController.java
@@ -1,13 +1,8 @@
 package com.bit.joe.shoppingmall.controller;
 
-import java.lang.reflect.Member;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/admin/member")
@@ -15,14 +10,5 @@ public class MemberController {
     @GetMapping
     public String index() {
         return "admin/member"; // Html 페이지로 이동
-    }
-
-    // JSON 데이터 반환하는 메서드
-    @GetMapping("/data")
-    @ResponseBody
-    public List<Member> getMemberData() {
-        List<Member> members = new ArrayList<>();
-
-        return members;
     }
 }
