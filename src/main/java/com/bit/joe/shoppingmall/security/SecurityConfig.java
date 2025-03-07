721ce32b4c6e719225862a3989183fe628c259cb
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-03-07 18:32:56 +0900

    refactor: reorganize import statements for consistency and clarity

--- src/main/java/com/bit/joe/shoppingmall/security/SecurityConfig.java
+++ src/main/java/com/bit/joe/shoppingmall/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.bit.joe.shoppingmall.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,14 +13,28 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 import com.bit.joe.shoppingmall.enums.UserRole;
+import com.bit.joe.shoppingmall.jwt.JWTUtil;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
 
     @Bean
     public static BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
