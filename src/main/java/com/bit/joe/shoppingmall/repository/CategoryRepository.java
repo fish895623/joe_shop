07bf7db7ca1738728815cfcb3f27b327f8f37d33
dangew <dangew99@gmail.com>
2025-03-10 14:26:56 +0900

    refactor: enhance Category management tests and DTO structure

--- src/main/java/com/bit/joe/shoppingmall/repository/CategoryRepository.java
+++ src/main/java/com/bit/joe/shoppingmall/repository/CategoryRepository.java
@@ -1,7 +1,11 @@
 package com.bit.joe.shoppingmall.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bit.joe.shoppingmall.entity.Category;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategoryName(String categoryName);
+}
