36fbf40b3dd0df6c6683c95cf3bc53e9b54431a0
KatherineJH <ohhomoya@gmail.com>
2025-03-10 21:28:47 +0900

    first commit
    first commit

--- src/main/java/com/bit/joe/shoppingmall/view/AdminController.java
+++ src/main/java/com/bit/joe/shoppingmall/view/AdminController.java
@@ -1,15 +1,36 @@
 package com.bit.joe.shoppingmall.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/insight")
-    public String insight() {
+    public String showDashboard(Model model) {
+        String sql = "SELECT u.gender, c.category_name AS categoryName, " +
+                "AVG(COALESCE(oi.price * oi.quantity, 0)) AS averageSales " +
+                "FROM users u " +
+                "JOIN orders o ON u.id = o.user_id " +
+                "JOIN order_items oi ON o.id = oi.order_id " +
+                "JOIN products p ON oi.product_id = p.product_id " +
+                "JOIN categories c ON p.category_id = c.category_id " +
+                "GROUP BY u.gender, c.category_name";
+
+        List<Map<String, Object>> averageSales = jdbcTemplate.queryForList(sql);
+        model.addAttribute("averageSales", averageSales);
+
         return "admin/insight";
     }
 
