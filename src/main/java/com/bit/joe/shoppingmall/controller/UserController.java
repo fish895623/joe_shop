f0d51afe2ef3aaee19ef44104d3ac6b8d9281db7
fish895623 <34264960+fish895623@users.noreply.github.com>
2025-03-10 17:23:08 +0900

    bsh-mypage-edit-and-save (#120)
    * feat: enhance user profile page with improved layout and validation
    
    * fix: update profile linkˆ
    
    * feat: implement user profile update functionality with validation and error handling
    
    * refactor: reorganize imports in UserController and UpdateUserInfoRequest for clarity

--- src/main/java/com/bit/joe/shoppingmall/controller/UserController.java
+++ src/main/java/com/bit/joe/shoppingmall/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bit.joe.shoppingmall.controller;
 
+import java.util.Objects;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bit.joe.shoppingmall.dto.UserDto;
+import com.bit.joe.shoppingmall.dto.request.UpdateUserInfoRequest;
 import com.bit.joe.shoppingmall.dto.response.Response;
 import com.bit.joe.shoppingmall.enums.UserRole;
 import com.bit.joe.shoppingmall.service.UserService;
@@ -31,15 +34,19 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/info")
-    public ResponseEntity<Response> getRole() {
-        UserDto user = new UserDto();
-        user.setEmail("admin");
-        user.setRole(UserRole.valueOf("ADMIN"));
-        // get user from user details
+    @PostMapping("/info")
+    public ResponseEntity<Response> getRole(
+            @RequestBody UserDto userDto,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        // load current authenticated user information
+
+        UserDto user = userService.getUserByEmail(userDto.getEmail()).getUser();
+
         log.info("User Info: {}", user);
 
-        return ResponseEntity.status(HttpStatus.OK).body(Response.builder().user(user).build());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.builder().status(200).user(user).build());
     }
 
     @GetMapping("/get-all")
@@ -99,6 +106,36 @@ public class UserController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userId, userDto));
+        // return success response with status code 200 (OK)
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Response> updateUser(@RequestBody UpdateUserInfoRequest userDto) {
+
+        UserDto user = userService.getUserByEmail(userDto.getEmail()).getUser();
+
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(
+                            Response.builder()
+                                    .status(401)
+                                    .message("Unauthorized(in controller): User is not logged in")
+                                    .build());
+            // return unauthorized response with status code 401 -> user is not logged in
+        }
+
+        Long userId = user.getId();
+
+        user.setName(userDto.getName());
+        user.setBirth(userDto.getBirth());
+        user.setPhone(userDto.getPhone());
+        user.setAddress(userDto.getAddress());
+        user.setGender(userDto.getGender());
+        if (Objects.equals(userDto.getConfirmPassword(), userDto.getNewPassword())) {
+            user.setPassword(userDto.getNewPassword());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userId, user));
         // return success response with status code 200 (OK)
     }
 
