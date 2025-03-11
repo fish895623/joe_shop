62f6f5e05da321deb7bcdefa6032d1ee04ee55d6
kyungmin <kyungmin@example.com>
2025-03-11 12:07:00 +0900

    Add cart-1

--- src/main/java/com/bit/joe/shoppingmall/view/CartHtmlController.java
+++ src/main/java/com/bit/joe/shoppingmall/view/CartHtmlController.java
@@ -1,14 +1,19 @@
 package com.bit.joe.shoppingmall.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/cart")
+@RequiredArgsConstructor
 public class CartHtmlController {
-    @GetMapping
-    public String cart() {
-        return "product/cart";
+
+    @GetMapping("/{userId}")
+    public String getCartPage(@PathVariable Long userId, Model model) {
+        model.addAttribute("userId", userId); // 사용자 ID를 모델에 추가하여 페이지에서 사용
+        return "product/cart"; // product/cart.html 페이지를 반환
     }
 }
