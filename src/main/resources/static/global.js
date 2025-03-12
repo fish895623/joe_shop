8591c80e92745df04b7130971cff585755267ace
dangew <dangew99@gmail.com>
2025-03-12 16:28:14 +0900

    fix: remove unnecessary blank lines in cart.html and global.js

--- src/main/resources/static/global.js
+++ src/main/resources/static/global.js
@@ -92,7 +92,6 @@ function getValuesFromLocalStorage(valueTitle) {
 
 // 쿠키를 이용해서 카트 데이터 가져오기 ========================================================
 async function getCart() {
-
   // 장바구니 데이터를 가져오기 위해 서버에 요청
   const cartData = await fetch("/api/cart/get", {
     method: "GET",
@@ -100,15 +99,14 @@ async function getCart() {
     headers: {
       Authorization: "Content-Type: application/json",
     },
-  })
-      .then((response) => {
-        // 장바구니 데이터를 가져오는데 실패하면 에러 페이지로 이동
-        if (!response.ok) {
-          console.error("Error fetching cart data:", response);
-          window.location.href = "/error";
-        }
-        return response.json();
-      });
+  }).then((response) => {
+    // 장바구니 데이터를 가져오는데 실패하면 에러 페이지로 이동
+    if (!response.ok) {
+      console.error("Error fetching cart data:", response);
+      window.location.href = "/error";
+    }
+    return response.json();
+  });
 
   localStorage.setItem("cart", JSON.stringify(cartData.cart));
 }
